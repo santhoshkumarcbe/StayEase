26c208f284859bead5dfbbed53b373403aee8afc
santhoshkumarcbe <santhoshkumarcbe35@gmail.com>
2026-04-09 17:21:30 +0530

    import addition

--- src/main/java/com/takehome/stayease/config/OpenApiConfig.java
+++ src/main/java/com/takehome/stayease/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.takehome.stayease.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
