435c67f1137d0fd24f0c66554ae0a92b4b501329
santhoshkumarcbe <santhoshkumarcbe35@gmail.com>
2026-04-09 17:03:24 +0530

    add swagger security components

--- src/main/java/com/takehome/stayease/controller/HotelController.java
+++ src/main/java/com/takehome/stayease/controller/HotelController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/api/hotels")
